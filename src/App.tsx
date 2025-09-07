a6decda5cac2143e96bc8da27d15071c244fd37b
Netanel kalir <netanelkalir@gmail.com>
2025-09-07 16:57:11 +0300

    post card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
+import Post from "./comp/Post";
+import "./App.css"
+import type { PostProps } from "./comp/Post";
+
+
+
+export const posts: PostProps[] = [{
+  img: "src/assets/logo.jpg",
+  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
+  likes: 20,
+  owner: "neta",
+  date: new Date().toLocaleString()
+}, {
+  img: "src/assets/logo.jpg",
+  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
+  likes: 10,
+  owner: "namoi",
+  date: new Date().toLocaleString()
+
+}, {
+  img: "src/assets/logo.jpg",
+  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
+  likes: 50,
+  owner: "netanel",
+  date: new Date().toLocaleString()
+
+}, {
+  img: "src/assets/logo.jpg",
+  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
+  likes: 100,
+  owner: "ali",
+  date: new Date().toLocaleString()
+
+}]
 
 export default function App() {
   return (
-    <div>
-      
-      
+    <div className="app">
+      <div className="posts">
+        {posts.map((post, index) => (
+          <Post key={index} {...post} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/comp/Post.tsx
+++ src/comp/Post.tsx
@@ -1,8 +1,26 @@
+import "./post.css"
+// import "src/assets/logo.jpg"
 
-export default function Post() {
-  return (
-    <div>
-        
-    </div>
-  )
+
+export type PostProps = {
+    img: string,
+    discrption: string,
+    likes: number,
+    owner: string,
+    date: string
+}
+export default function Post(post: PostProps) {
+    return (
+        <>
+            <div className="post" >
+                <img className="img" src={post.img} alt="" />
+                <h2 className="discrption">{`discrption: ${post.discrption}`}</h2>
+                <div className="details">
+                    <p >{`like: ${post.likes}`}</p>
+                    <p >{`owner: ${post.owner}`}</p>
+                    <p >{`date: ${post.date}`}</p>
+                </div>
+            </div>
+        </>
+    )
 }
