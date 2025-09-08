37c2b95b55633a4f1920405edc3026a47304c7e3
Netanel kalir <netanelkalir@gmail.com>
2025-09-08 15:19:05 +0300

    create post added in dal

--- back/DAL/CRUD.js
+++ back/DAL/CRUD.js
@@ -7,16 +7,24 @@ export async function getAllPostsDal() {
         console.log('File content:', data);
         return data
     } catch (err) {
-        console.error('Error reading file:', err);
+        console.error("DAL Error: Error reading file: ", err);
     }
 }
 
-export async function createPostDal() {
+export async function createPostDal(newPost) {
     try {
-        
+        let posts = [];
+        const data = await fs.readFile('data/posts.txt', 'utf-8');
+        posts = data ? JSON.parse(data) : [];
 
+        posts.push(newPost);
+
+        await fs.writeFile('data/posts.txt', JSON.stringify(posts, null, 2), 'utf-8');
+
+        return newPost;
     } catch (error) {
-
+        console.error("DAL Error: fild to creat new post: ", error);
+        throw error;
     }
 }
 
