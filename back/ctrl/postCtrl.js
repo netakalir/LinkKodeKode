aafe292b08625507dd1728c397b05eb63e8e5546
Netanel kalir <netanelkalir@gmail.com>
2025-09-08 16:18:23 +0300

    valid

--- back/ctrl/postCtrl.js
+++ back/ctrl/postCtrl.js
@@ -3,7 +3,7 @@ import { getAllPostsDal, createPostDal } from "../DAL/CRUD.js";
 export async function getAllPosts(req, res) {
     try {
         const result = await getAllPostsDal()
-        res.json("post exsist: ", result)
+        res.json("all posts : ", result)
     } catch (error) {
         res.json("CTRL Error: post isn't exsist");
     }
@@ -15,7 +15,7 @@ export async function createPost(req, res) {
         const { discrption, owner } = req.body
         const post = {
             id: result.length + 1,
-            img: `htpp//localhost:3000/images/${id}.jpg`,
+            img: `htpp//localhost:3000/images/${result.length + 1}.png`,
             discrption: discrption,
             likes: 0,
             owner: owner,
@@ -23,8 +23,13 @@ export async function createPost(req, res) {
         }
         try {
             if (post.owner !== undefined && post.discrption !== undefined) {
-                await createPostDal(post)
-                res.json("post created successfully: ", post.owner)
+                if(result.length <= 5){
+                    await createPostDal(post)
+                    res.json("post created successfully: ", post)
+                }
+                else{
+                    res.json("We are unable to add another post.")
+                }
             }
             else {
                 res.json("insert all part of post")
