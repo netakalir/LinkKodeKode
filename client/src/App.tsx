521a0ba906b6342a1e4bac117e37f5fb39897a5b
Netanel kalir <netanelkalir@gmail.com>
2025-09-08 17:26:06 +0300

    add useRouts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,69 +1,20 @@
-import Post from "./components/application-layout/Post";
 import "./App.css"
-import type { PostProps } from "./components/application-layout/Post";
-import Header from "./components/application-layout/Header";
+// import type { PostProps } from "./components/application-layout/Post";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/homePage";
+import NewPostPage from "./pages/newPostPage";
 
 
 
-export const posts: PostProps[] = [{
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 20,
-  owner: "neta",
-  date: new Date().toLocaleString()
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 10,
-  owner: "namoi",
-  date: new Date().toLocaleString()
 
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 50,
-  owner: "netanel",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}]
 
 export default function App() {
   return (
-    <div className="app">
-      <Header/>
-      <div className="posts">
-        {posts.map((post, index) => (
-          <Post key={index} {...post} />
-        ))}
-      </div>
+    <div>
+    <Routes>
+      <Route path="/"element={<HomePage />}/>
+      <Route path="/newPost"element={<NewPostPage />}/>
+    </Routes>
     </div>
   )
 }
