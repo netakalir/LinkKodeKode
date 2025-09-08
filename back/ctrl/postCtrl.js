a3540a083b09c8949a5dcf58e4ad715715b77b26
Netanel kalir <netanelkalir@gmail.com>
2025-09-08 15:18:41 +0300

    create post added

--- back/ctrl/postCtrl.js
+++ back/ctrl/postCtrl.js
@@ -1,11 +1,33 @@
-import { getAllPostsDal } from "../DAL/CRUD.js";
+import { getAllPostsDal, createPostDal } from "../DAL/CRUD.js";
 
-export async function getAllPosts(req,res) {
+export async function getAllPosts(req, res) {
     try {
         const result = await getAllPostsDal()
-        console.log("post exsist");
-        res.json(result)
+        res.json("post exsist: ", result)
     } catch (error) {
-        console.log("post isn't exsist");
+        res.json("CTRL Error: post isn't exsist");
+    }
+}
+
+export async function createPost(req, res) {
+    try {
+        const { discrption, owner } = req.body
+        const post = {
+            id: Date.now(),
+            img: "htpp//localhost:3000/images/logo.jpg",
+            discrption: discrption,
+            likes: 0,
+            owner: owner,
+            date: new Date().toLocaleString()
+        }
+        try {
+            await createPostDal(post)
+            res.json("post created successfully: ", post)
+        } catch (error) {
+            res.json("CTRL Error: fild to create new post:", error)
+        }
+        
+    } catch (error) {
+        res.json("CTRL Error: error receiving data from the client",error)
     }
 }
