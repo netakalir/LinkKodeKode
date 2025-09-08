39366159eb109f1a709bdc172b4e29551cc1bdcf
Netanel kalir <netanelkalir@gmail.com>
2025-09-08 15:48:58 +0300

    add valid in create func

--- back/ctrl/postCtrl.js
+++ back/ctrl/postCtrl.js
@@ -10,24 +10,30 @@ export async function getAllPosts(req, res) {
 }
 
 export async function createPost(req, res) {
+    const result = await getAllPostsDal()
     try {
         const { discrption, owner } = req.body
         const post = {
-            id: Date.now(),
-            img: "htpp//localhost:3000/images/logo.jpg",
+            id: result.length + 1,
+            img: `htpp//localhost:3000/images/${id}.jpg`,
             discrption: discrption,
             likes: 0,
             owner: owner,
             date: new Date().toLocaleString()
         }
         try {
-            await createPostDal(post)
-            res.json("post created successfully: ", post)
+            if (post.owner !== undefined && post.discrption !== undefined) {
+                await createPostDal(post)
+                res.json("post created successfully: ", post.owner)
+            }
+            else {
+                res.json("insert all part of post")
+            }
         } catch (error) {
             res.json("CTRL Error: fild to create new post:", error)
         }
-        
+
     } catch (error) {
-        res.json("CTRL Error: error receiving data from the client",error)
+        res.json("CTRL Error: error receiving data from the client", error)
     }
 }
