497807adbb25603f8cc1144f2bdf920cdfc93383
Netanel kalir <netanelkalir@gmail.com>
2025-09-09 09:36:32 +0300

    add rest api for create new post

--- client/src/api.fetch.ts
+++ client/src/api.fetch.ts
@@ -1,3 +1,5 @@
+import { type PostProps } from "./components/application-layout/Post";
+
 export async function getAllPost() {
     try {
         const response = await fetch("http://localhost:4010/linkKodeKode/getAllPosts", {
@@ -15,3 +17,20 @@ export async function getAllPost() {
     }
 }
 
+export async function createNewPost(post: PostProps) {
+    try {
+        const response = await fetch("http://localhost:4010/linkKodeKode/createPost", {
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(post)
+        })
+        const data = await response.json()
+        console.log(data);
+        return data
+    } catch (error) {
+        console.log(error);
+        return null
+    }
+}
