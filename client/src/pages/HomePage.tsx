7c6b91a1efadd547d94d0c4b14fa2b5630edd9e4
Netanel kalir <netanelkalir@gmail.com>
2025-09-09 11:18:51 +0300

    chenge pages

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,58 +1,23 @@
+import { useEffect, useState } from "react";
 import Header from "../components/application-layout/Header";
 import Post, { type PostProps } from "../components/application-layout/Post";
 import "./style.css"
+import { getAllPost } from "../api.fetch";
+// import NewPost from "../components/newPost";
+import { useNavigate } from "react-router";
 
-export const posts: PostProps[] = [{
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 20,
-  owner: "neta",
-  date: new Date().toLocaleString()
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 10,
-  owner: "namoi",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 50,
-  owner: "netanel",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}, {
-  img: "src/assets/logo.jpg",
-  discrption: "ugvihkbkjbvkglihkbk.h gcluivbk",
-  likes: 100,
-  owner: "ali",
-  date: new Date().toLocaleString()
-
-}]
 
 export default function HomePage() {
+    const [posts, setPosts] = useState<PostProps[]>([])
+    let navigate = useNavigate()
+    useEffect(() => {
+        const fetchPost = async () => {
+            const result = await getAllPost();
+            setPosts(result)
+            console.log(result);
+        }
+        fetchPost()
+    }, [])
     return (
         <div className="page">
             <Header />
@@ -61,6 +26,7 @@ export default function HomePage() {
                     <Post key={index} {...post} />
                 ))}
             </div>
+            <button onClick={() => navigate("/NewPostPage")}>newPost</button>
         </div>
     )
 }
--- client/src/pages/NewPostPage.tsx
+++ client/src/pages/NewPostPage.tsx
@@ -1,11 +1,12 @@
 import Header from "../components/application-layout/Header";
+import NewPost from "../components/newPost"
 import "./style.css"
 
 export default function NewPostPage() {
   return (
     <div className="page">
         <Header />
-
+        <NewPost />
     </div>
   )
 }
